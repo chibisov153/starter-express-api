aca81eb5700044813595f223b768fe8e6e3f6c10
chibisov153 <chibisov153@gmail.com>
2023-10-28 13:51:19 +0700

    up data 28.10.1

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express')
+const { createServer } = require('node:http')
+const { join } = require('node:path')
+const { Server } = require('socket.io')
+
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const server = createServer(app)
+const io = new Server(server)
+
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
+
+app.get('/', (req, res) => {
+	res.sendFile(join(__dirname, 'index.html'))
 })
-app.listen(process.env.PORT || 3000)
+
+io.on('connection', socket => {
+	console.log('Connection', socket.id)
+	socket.on('chat message', msg => {
+		console.log('message: ' + msg)
+		io.emit('chat message', msg)
+	})
+})
+
+server.listen(process.env.PORT || 3000)
